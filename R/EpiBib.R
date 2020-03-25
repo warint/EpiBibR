7e5b1e33fcedf76cf7c0f43bfb269f5301e4c6d2
Martin Paquette <paquettem@nuance-r.com>
2020-03-25 12:45:42 +0000

    u

---
+++ R/EpiBib.R
@@ -0,0 +1,183 @@
+
+
+load("EpiBib_data.RData")
+
+# Function 1: Retrieving Bibliographic Data
+
+#' EpiBib_df
+#'
+#' @Description this function allows you to retrieve the complete medical bibliographic dataframe.
+#'
+#' @return The complete bibliographic dataframe
+#' @export
+#'
+#' @examples
+#' df <- EpiBib_df()
+
+
+
+EpiBib_df <- function() {
+    EpiBib_data
+}
+
+
+
+# Function 2: Authors
+
+#' EpiBib_author
+#'
+#' @description This function allows you to search references by authors.
+#'
+#' @param author The name of the author
+#'
+#'
+#' @return dataframe with only the references from the selected author
+#' @export
+#'
+#' @examples
+#' df <- EpiBib_author("Yang")
+#' df <- EpiBib_author("Ya")
+#' df <- EpiBib_author("Yang")
+
+EpiBib_author <- function(author) {
+  if (missing(author)) {
+    EpiBib_data
+  } else {
+    EpiBib_data[grep(author,  EpiBib_data$AU, ignore.case = TRUE), ]
+  }
+}
+
+
+
+
+# Function 3: Authors'country of origin
+
+#' EpiBib_country
+#'
+#' @description This function allows you to search references by authors' country of origin.
+#' Be careful. countries can be identified differently. For exemple, United Kingdom vs England.
+#'
+#' @param country The name of the country
+#'
+#' @return dataframe with only the references from the selected country.
+#' @export
+#'
+#' @examples
+#' df <- EpiBib_country("Canada")
+#' df <- EpiBib_country("china")
+#' df <- EpiBib_country("England")
+
+EpiBib_country <- function(country) {
+  if (missing(country)) {
+    EpiBib_data
+  } else {
+    EpiBib_data[grep(country, EpiBib_data$AU_CO, ignore.case = TRUE), ]
+  }
+}
+
+
+
+# Function 4: Title
+
+#' EpiBib_title
+#'
+#' @description This function allows you to search references by keywords in the title.
+#'
+#' @param title keyword in the title
+#'
+#' @return dataframe with only the references including the selected word in the title.
+#' @export
+#'
+#' @examples
+#' df <- EpiBib_title("pandemic")
+#' df <- EpiBib_title("coronaVir")
+
+
+EpiBib_title <- function(title) {
+  if (missing(title)) {
+    EpiBib_data
+  } else {
+    EpiBib_data[grep(title, EpiBib_data$TI, ignore.case = TRUE), ]
+  }
+}
+
+
+
+
+# Function 5: Year
+
+#' EpiBib_year
+#'
+#' @description This function allows you to search references by year.
+#'
+#' @param year A year
+#'
+#' @return dataframe with only the references including the selected year.
+#' @export
+#'
+#' @examples
+#' df <- EpiBib_year("2017")
+
+
+EpiBib_year <- function(year) {
+  if (missing(year)) {
+    EpiBib_data
+  } else {
+    EpiBib_data[grep(year, EpiBib_data$PY, ignore.case = TRUE), ]
+  }
+}
+
+
+
+# Function 6: Journal
+
+#' EpiBib_journal
+#'
+#' @description This function allows you to search references by journal.
+#' Be careful, journals' name are only avalailable in an abbreviated form.
+#'
+#'
+#' @param journal The name of the journal
+#'
+#' @return dataframe with only the references including the selected journal.
+#' @export
+#'
+#' @examples
+#' df <- EpiBib_journal("med")
+#' df <- EpiBib_journal("bio")
+
+
+EpiBib_journal <- function(journal) {
+  if (missing(journal)) {
+    EpiBib_data
+  } else {
+    EpiBib_data[grep(journal, EpiBib_data$SO, ignore.case = TRUE), ]
+  }
+}
+
+
+
+
+# Function 7: Abstract
+
+#' EpiBib_abstract
+#'
+#' @description This function allows you to search references by keywords in the title.
+#'
+#' @param abstract keyword in the abstract
+#'
+#' @return dataframe with only the references including the selected word in the title.
+#' @export
+#'
+#' @examples
+#' df <- EpiBib_abstract("coronav")
+#' df <- EpiBib_abstract("issue")
+#'
+
+EpiBib_abstract <- function(abstract) {
+  if (missing(abstract)) {
+    EpiBib_data
+  } else {
+    EpiBib_data[grep(abstract, EpiBib_data$AB, ignore.case = TRUE), ]
+  }
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+us
+# EpiBibR
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of EpiBibR is to ...
+
+## Installation
+
+You can install the released version of EpiBibR from [CRAN](https://CRAN.R-project.org) with:
+
+``` r
+install.packages("EpiBibR")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(EpiBibR)
+## basic example code
+```
+
